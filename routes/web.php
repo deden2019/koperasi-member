cf31812bf2f96ee0f80299938fc9357b59fdcb63
deden2019 <dedensaputra123@gmail.com>
2026-09-25 22:42:47 +0800

    update landing

--- routes/web.php
+++ routes/web.php
@@ -10,43 +10,40 @@ use App\Http\Controllers\CardController;
 use App\Http\Controllers\PiutangController;
 use App\Http\Controllers\PembayaranController;
 
-Route::get('/', [AuthController::class, 'index']);
+// 1. Landing Page Utama
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 2. Rute Login & Logout Portal Member
+Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-
+// 3. Area Khusus Member Terautentikasi
 Route::middleware('member.auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index']);
 
+    // Transaksi
     Route::get('/transaksi', [TransaksiController::class, 'index']);
-
     Route::get('/transaksi/{jualId}', [TransaksiController::class, 'show']);
 
+    // Profile
     Route::get('/profile', [ProfileController::class, 'index']);
     Route::get('/profile/edit', [ProfileController::class, 'edit']);
+    Route::post('/profile/update', [ProfileController::class, 'update']);
 
-Route::post('/profile/update', [ProfileController::class, 'update']);
-
-
+    // Ubah PIN
     Route::get('/change-pin', [PinController::class, 'index']);
-Route::post('/change-pin', [PinController::class, 'update']);
+    Route::post('/change-pin', [PinController::class, 'update']);
 
-Route::get('/change-pin', [PinController::class, 'index']);
+    // Kartu Anggota
+    Route::get('/card', [CardController::class, 'index']);
 
-Route::post('/change-pin', [PinController::class, 'update']);
-Route::get(
-    '/card',
-    [CardController::class, 'index']
-);
-
-Route::get('/piutang', [PiutangController::class, 'index']);
-
-Route::get('/piutang/terbayar', [PiutangController::class, 'terbayar']);
-
-Route::get(
-    '/riwayat-pembayaran',
-    [PembayaranController::class, 'index']
-);
+    // Piutang & Pembayaran
+    Route::get('/piutang', [PiutangController::class, 'index']);
+    Route::get('/piutang/terbayar', [PiutangController::class, 'terbayar']);
+    Route::get('/riwayat-pembayaran', [PembayaranController::class, 'index']);
 
 });
