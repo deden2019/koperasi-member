b119bcf7004424966abf4b1e9f030c42144b6252
deden2019 <dedensaputra123@gmail.com>
2026-09-26 05:14:33 +0800

    landingpage

--- app/Http/Middleware/MemberAuth.php
+++ app/Http/Middleware/MemberAuth.php
@@ -11,7 +11,7 @@ class MemberAuth
     public function handle(Request $request, Closure $next): Response
     {
         if (!session('customer_id')) {
-            return redirect('/');
+            return redirect('/login');
         }
 
         return $next($request);
